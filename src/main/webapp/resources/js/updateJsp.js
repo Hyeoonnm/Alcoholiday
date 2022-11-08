07fa3ec82233d316dcae28e3ea4e89167d2eea9d
KangJaeHyeon <rkdwogus77@gmail.com>
2022-11-08 15:57:06 +0900

    update.js에 새로고침 추가

--- src/main/webapp/resources/js/updateJsp.js
+++ src/main/webapp/resources/js/updateJsp.js
@@ -67,7 +67,7 @@ function deleteImg(obj) {
         data : param,            // Json 형식의 데이터이다.
         success : function(res){ // 비동기통신의 성공일경우 success콜백으로 들어옵니다. 'res'는 응답받은 데이터이다.
             // 응답코드 > 0000
-
+        location.reload();
         },
         error : function(XMLHttpRequest, textStatus, errorThrown){ // 비동기 통신이 실패할경우 error 콜백으로 들어옵니다.
             alert("통신 실패.")
